3c5c29e601fe4314c4131e6da9fc2ea73d1d0ba6
15951595 <shun19871001@gmail.com>
2020-08-23 22:56:58 +0900

    type safety

--- src/stores/actions.ts
+++ src/stores/actions.ts
@@ -2,18 +2,18 @@ import asyncRequest from './services/asyncRequest'
 
 // actions
 export default {
-    getBTC: ({ commit }: { commit: void }): Promise<object> => {
+    getBTC: ({ commit }: { commit: any }): Promise<object> => {
         return new Promise<object>((resolve: () => void, reject: () => void) => {
             const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
             asyncRequest(url)
-                .then((res: object) => {
+                .then((res: AxiosResponse): void => {
                     console.log('----------Axios Success----------')
                     commit('setBTCUSD', res.data.bpi.USD.rate)
                     commit('setBTCGBP', res.data.bpi.GBP.rate)
                     commit('setBTCEUR', res.data.bpi.EUR.rate)
                     resolve()
                 })
-                .catch((err: any) => {
+                .catch((err: any): void => {
                     console.log('----------Axios Err----------')
                     console.log(err)
                     reject()
--- src/stores/services/asyncRequest.ts
+++ src/stores/services/asyncRequest.ts
@@ -2,10 +2,10 @@ import axios from 'axios'
 //axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 export default function asyncRequest(endPoint: string): Promise<object> {
-    return new Promise<object>((resolve: (value?: object) => any, reject: (reason?: any) => any) => {
+    return new Promise<object>((resolve: (value?: any) => any, reject: (reason?: any) => any) => {
         axios
             .get(endPoint, {})
-            .then((res: object): Promise<object> => resolve(res))
-            .catch((err: any): any => reject(err))
+            .then((res: any): Promise<object> => resolve(res))
+            .catch((err: any): Promise<object> => reject(err))
     })
 }
